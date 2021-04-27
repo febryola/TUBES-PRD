6efa0d832ffb51b95f5208c941dfe7d0108c2dd2
febryola <febryola0402@gmail.com>
2021-04-27 18:50:20 +0700

    menambahkan judul

--- TUBESPRD.py
+++ TUBESPRD.py
@@ -1,9 +1,24 @@
 #Kodingan buat TUBES PRD
 #Baru inputnya doang gaess, belom kebayang implementasi rumus2nyaaa :nnnn
 
+
+    # Diambil dari : https://patorjk.com/software/taag/#p=display&f=Big&t=Kantong%20Ajaib
+print("""
+
+  ______            _            _            _      _____ _                _ _   
+ |  ____|          (_)          | |          | |    / ____(_)              (_) |  
+ | |__   __ _ _   _ ___   ____ _| | ___ _ __ | |_  | |     _ _ __ ___ _   _ _| |_ 
+ |  __| / _` | | | | \ \ / / _` | |/ _ \ '_ \| __| | |    | | '__/ __| | | | | __|
+ | |___| (_| | |_| | |\ V / (_| | |  __/ | | | |_  | |____| | | | (__| |_| | | |_ 
+ |______\__, |\__,_|_| \_/ \__,_|_|\___|_| |_|\__|  \_____|_|_|  \___|\__,_|_|\__|
+           | |                                                                    
+           |_|                                                               
+""")
+                                                                 
 jumlah_node = int(input("Masukkan jumlah node pada rangkaian: "))
 R = int(input("Masukkan jumlah resistor           : "))
 V = int(input("Masukkan jumlah voltase            : "))
+print()
 R1=[]
 V1=[]
 for r in range(R):
@@ -12,7 +27,7 @@ for r in range(R):
 for resistor in R1:
     print("Array dari resistor adalah", R1)
     break
-
+print()
 for v in range(V):
     V2=int(input("Masukkan nilai voltase ke-{} (volt) : ".format(v+1)))
     V1.append(V2)
