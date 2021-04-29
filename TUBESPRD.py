69d56de40430324fde2224428f45f32e2d476f1f
febryola <febryola0402@gmail.com>
2021-04-29 20:56:30 +0700

    menambah rangkaian

--- TUBESPRD.py
+++ TUBESPRD.py
@@ -81,7 +81,73 @@ print("""
 
 
 """)
-                                                            
+
+while True:
+    pilihan = int(input("Silakan Inputkan Nomor Rangkaian Yang Ingin Anda Selesaikan: "))
+    if (pilihan==1):
+        print(""" Berikut bentuk rangkaian yang akan anda selesaikan
+    n1____(R1)____.____(R2)____.n-      Keterangan:
+    |            |                     R1 = nilai resistor 1
+    |            |                     R2 = nilai resistor 2
+    |           (R3)                   R3 = nilai resistor 3
+    |            |                     R4 = nilai resistor 4
+    (V)          |____________.n+      (V)= nilai tegangan sumber bebas
+    |            |
+    |           (R4)
+    |            |
+    |____________|""")
+
+    elif (pilihan==2):
+        print(""" Berikut bentuk rangkaian yang akan anda selesaikan
+    
+     ____(R1)____.____(R2)____.a      Keterangan:
+    |            |                    R1 = nilai resistor 1
+    |            |                    R2 = nilai resistor 2  
+    <V>          (R3)                  R3 = nilai resistor 3
+    |            |                    <v>= nilai tegangan sumber tidak bebas
+    |____________|____________.b
+        """)
+
+    elif (pilihan==3):
+
+        print(""" Berikut bentuk rangkaian yang akan anda selesaikan
+     ____(R1)____<V>__________.a      Keterangan:
+    |                   |             R1 = nilai resistor 1
+    |                   |             R2 = nilai resistor 2
+    (R2)                (R3)           R3 = nilai resistor 3
+    |                   |             <v>= nilai tegangan sumber tidak bebas
+    |___________________|_____.b
+        """)
+
+    elif (pilihan==4):
+        print(""" Berikut bentuk rangkaian yang akan anda selesaikan
+        ____(R1)____.____(R2)____        Keterangan:
+    |            |            |       R1 = nilai resistor 1
+    |            a            |       R2 = nilai resistor 2  
+    (V)                       <V>      (V)= nilai tegangan sumber bebas
+    |            b            |       <v>= nilai tegangan sumber tidak bebas
+    |____________|____________|
+        """)
+
+    elif (pilihan==5):
+        print("""berikut bentuk rangkaian yang akan anda selesaikan
+        
+        ______________(R2)____.________________.a      Keterangan:
+    |        |                |      |              R1 = nilai resistor 1
+    |       (R3)            (R4)     |              R2 = nilai resistor 2  
+    (V1)      |________________|     (V3)            R3 = nilai resistor 3 
+    |                |               |              R4 = nilai resistor 4 
+    |              (R5)              |              R5 = nilai resistor 5 
+    |                |               |              (V1)= nilai tegangan sumber bebas 1
+    (R1)              |              (R5)            (V2)= nilai tegangan sumber bebas 2
+    |              (V2)              |              (V3)= nilai tegangan sumber bebas 3
+    |________________|_______________|______.b
+        """)
+    else:
+        print()
+        print("Masukan tidak valid, silakan pilih dari 1-5 rangkaian")
+        
+
 jumlah_node = int(input("Masukkan jumlah node pada rangkaian: "))
 R = int(input("Masukkan jumlah resistor           : "))
 V = int(input("Masukkan jumlah voltase            : "))
