193d7afb4e14fec9616033cf55276bb871613d25
febryola <febryola0402@gmail.com>
2021-05-04 14:31:16 +0700

    mempersingkat kode

--- test.py
+++ test.py
@@ -58,110 +58,12 @@ def byebye(): # Fungsi yang mengeluarkan pesan bye bye jika program telah selesa
                     """)
     print(Style.RESET_ALL)
 
-def program():
-    print("""\033[92m=======================Silakan Pilih Menu di Bawah ini====================
-**************************************************************************\033[0m""")
-    print()
-    print("=================Berikut Menu Yang Tersedia Pada Program Ini==============")
-    print(Fore.BLACK+""+ Back.WHITE+"") #MEMBERI WARNA
-    print("""1. Help (Petunjuk Sistem)
-2. Simulasi Rangkaian    """) 
-    print(Style.RESET_ALL +Fore.WHITE+ "")#MERESET WARNA PROGRAM
-    print("**************************************************************************")
-    print("==========================================================================")
-
-#PROGRAM UTAMA
-    while True:
-        print(Fore.RED+"")
-        menu = int(input("Masukan nomor menu yang ingin dilakukan: "))
-        print(Style.RESET_ALL)
-        if menu==1:
-            print("""
-==================Berikut Petunjuk Penggunaan Sistem Ini==================
-**************************************************************************
-
-1. Sistem ini adalah sistem yang dibuat untuk para engineer yang ingin
-   menyelesaikan permasalahan mengenai equivalent circuit
-2. Pada sistem ini terdapat 5 jenis rangkaian equivalen
-3. Anda tinggal memasukkan nilai resistor dan voltasenya 
-4. Hasil akhir dari program ini adalah berupa nilai Rth,Vth,dan Ith
-5. Setelah itu Program akan selesai
-
-**************************************************************************
-==========================================================================""")
-            print()
-            break
-        elif menu==2:
-            print(Fore.BLACK+""+Back.WHITE+"")
-            print("""
+def rangkaian(): #Fungsi yang memanggil rangkaian
+    print(Fore.BLACK+""+Back.WHITE+"")
+    print("""
 1. Rangkaian equivalent pertama""")
-            print(Style.RESET_ALL)
-            print("""
-   \033[41mn1\033[0m____\033[36m(R1)\033[0m____.____\033[36m(R2)\033[0m____.\033[41mn-\033[0m      Keterangan:
-    |            |                     R1 = nilai resistor 1
-    |            |                     R2 = nilai resistor 2
-    |           \033[36m(R3)\033[0m                   R3 = nilai resistor 3
-    |            |                     R4 = nilai resistor 4
-   \033[93m(V)\033[0m           |                     (V)= nilai tegangan sumber bebas
-    |            |
-    |           \033[36m(R4)\033[0m
-    |            |
-    |____________|____________.\033[41mn+\033[0m  """)
-            print()
-            print("""
-2. Rangkaian equivalent kedua
-            
-     ____(R1)____.____(R2)____.a      Keterangan:
-    |            |                    R1 = nilai resistor 1
-    |            |                    R2 = nilai resistor 2  
-   <V>          (R3)                  R3 = nilai resistor 3
-    |            |                    <v>= nilai tegangan sumber tidak bebas
-    |____________|____________.b
-            
-3. Rangkaian equivalent ketiga
-
-     ____(R1)____<V>__________.a      Keterangan:
-    |                   |             R1 = nilai resistor 1
-    |                   |             R2 = nilai resistor 2
-   (R2)                (R3)           R3 = nilai resistor 3
-    |                   |             <v>= nilai tegangan sumber tidak bebas
-    |___________________|_____.b
-
-
-4. Rangkaian equivalent keempat
-            
-     ____(R1)____.____(R2)____        Keterangan:
-    |            |            |       R1 = nilai resistor 1
-    |            a            |       R2 = nilai resistor 2  
-   (V)                       <V>      (V)= nilai tegangan sumber bebas
-    |            b            |       <v>= nilai tegangan sumber tidak bebas
-    |____________|____________|
-
-5. Rangkaian equivalent kelima
-
-     _______________(R2)____________________.a      Keterangan:
-    |        |                |      |              R1 = nilai resistor 1
-    |       (R3)            (R4)     |              R2 = nilai resistor 2  
-   (V1)      |________________|     (V3)            R3 = nilai resistor 3 
-    |                |               |              R4 = nilai resistor 4 
-    |              (R5)              |              R5 = nilai resistor 5 
-    |                |               |             (V1)= nilai tegangan sumber bebas 1
-   (R1)              |              (R5)           (V2)= nilai tegangan sumber bebas 2
-    |              (V2)              |             (V3)= nilai tegangan sumber bebas 3
-    |________________|_______________|______.b
-
-            """)
-            break
-        else:
-            print("Silakan, Masukan Menu yang valid")
-
-    lanjut = input("Apakah anda ingin melakukan simulasi rangkaian ini (Y/N): ")
-    if lanjut=="Y" or lanjut=="y":
-        print(Fore.BLACK+""+Back.WHITE+"")
-        print("""
-1. Rangkaian equivalent pertama""")
-        print(Style.RESET_ALL)
-        print("""
+    print(Style.RESET_ALL)
+    print("""
        \033[41mn1\033[0m____\033[36m(R1)\033[0m____.____\033[36m(R2)\033[0m____.\033[41mn-\033[0m      Keterangan:
         |            |                     R1 = nilai resistor 1
         |            |                     R2 = nilai resistor 2
@@ -173,7 +75,7 @@ def program():
         |            |
         |____________|____________.\033[41mn+\033[0m  """)
         #Ga perlu diubah ya gaes yg udah aku edit please, di outputnya rapi kok
-        print("""
+    print("""
 2. Rangkaian equivalent kedua
             
          ____(R1)____.____(R2)____.a      Keterangan:
@@ -215,8 +117,52 @@ def program():
         |              (V2)              |             (V3)= nilai tegangan sumber bebas 3
         |________________|_______________|______.b
 
-            """)
+        """)
 
+
+def program():
+    print("""\033[92m=======================Silakan Pilih Menu di Bawah ini====================
+**************************************************************************\033[0m""")
+    print()
+    print("=================Berikut Menu Yang Tersedia Pada Program Ini==============")
+    print(Fore.BLACK+""+ Back.WHITE+"") #MEMBERI WARNA
+    print("""1. Help (Petunjuk Sistem)
+2. Simulasi Rangkaian    """) 
+    print(Style.RESET_ALL +Fore.WHITE+ "")#MERESET WARNA PROGRAM
+    print("**************************************************************************")
+    print("==========================================================================")
+
+#PROGRAM UTAMA
+    while True:
+        print(Fore.RED+"")
+        menu = int(input("Masukan nomor menu yang ingin dilakukan: "))
+        print(Style.RESET_ALL)
+        if menu==1:
+            print("""
+==================Berikut Petunjuk Penggunaan Sistem Ini==================
+**************************************************************************
+
+1. Sistem ini adalah sistem yang dibuat untuk para engineer yang ingin
+   menyelesaikan permasalahan mengenai equivalent circuit
+2. Pada sistem ini terdapat 5 jenis rangkaian equivalen
+3. Anda tinggal memasukkan nilai resistor dan voltasenya 
+4. Hasil akhir dari program ini adalah berupa nilai Rth,Vth,dan Ith
+5. Setelah itu Program akan selesai
+
+**************************************************************************
+==========================================================================""")
+            print()
+            break
+        elif menu==2:
+            rangkaian()
+            break
+        else:
+            print("Silakan, Masukan Menu yang valid")
+
+    lanjut = input("Apakah anda ingin melakukan simulasi rangkaian ini (Y/N): ")
+    if lanjut=="Y" or lanjut=="y":
+        rangkaian()
+        
         while True:
             pilihan = int(input("Silakan Inputkan Nomor Rangkaian Yang Ingin Anda Selesaikan: "))
             if (pilihan==1):
@@ -302,7 +248,7 @@ def program():
                     print("""
 ****************************************************************************
 =========================PROGRAM ANDA TELAH SELESAI=========================""")
-                    print(Fore.LIGHTYELLOW_EX+"")
+                    print(Fore.LIGHTYELLOW_EX+"") #memberikan warna kuning pada huruf byebye
                     byebye() #Mengeluarkan pesan bye bye ke sistem
                     break
 
